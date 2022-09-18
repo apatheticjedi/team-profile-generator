eaa653cc85018c5386bcb7edd4a672fac8f81e53
Duder <thedudeabides1981@hotmail.com>
2022-09-18 16:44:44 -0700

    created index questions

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const templateData = require('./src/page-template')
+const templateData = require('./src/page-template');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-const employeeQuestions = [
+const employeeInfo = [];
+
+const managerQuestion = [
     {
         type: 'input',
         name: 'name',
-        message: "Enter the Employee's name:"
+        message: "Enter the Manager's name:"
     },
     {
         type: 'input',
         name: 'id',
-        message: "Enter the Employee's ID number:"
+        message: "Enter the Manager's ID number:"
     },
     {
         type: 'input',
         name: 'email',
-        message: "Enter the Employee's email address:"
-    }
-];
-
-const managerQuestion = [
+        message: "Enter the Manager's email address:"
+    },
     {
         type: 'input',
         name: 'officeNumber',
@@ -31,6 +33,21 @@ const managerQuestion = [
 const engineerQuestion = [
     {
         type: 'input',
+        name: 'name',
+        message: "Enter the Engineer's name:"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter the Engineer's ID number:"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter the Engineer's email address:"
+    },
+    {
+        type: 'input',
         name: 'github',
         message: "Enter the Engineer's GitHub username:"
     }
@@ -39,52 +56,67 @@ const engineerQuestion = [
 const internQuestion = [
     {
         type: 'input',
+        name: 'name',
+        message: "Enter the Intern's name:"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter the Intern's ID number:"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter the Intern's email address:"
+    },
+    {
+        type: 'input',
         name: 'school',
         message: "What school does this intern attend?"
     }
 ];
 
-const addInfo = [
-    {
+const promptManager = () => {
+    inquirer.prompt(managerQuestion)
+        .then(managerInfo => {
+            const manager = new Manager(managerInfo.name, managerInfo.id, managerInfo.email, managerInfo.officeNumber);
+            employeeInfo.push(manager);
+            console.log(employeeInfo);
+        }).then(promptAdd)
+}
+
+const promptEngineer = () => {
+    inquirer.prompt(engineerQuestion)
+        .then(engineerInfo => {
+            const engineer = new Engineer(engineerInfo.name, engineerInfo.id, engineerInfo.email, engineerInfo.github);
+            employeeInfo.push(engineer);
+        }).then(promptAdd)
+}
+
+const promptIntern = () => {
+    inquirer.prompt(internQuestion)
+        .then(internInfo => {
+            const intern = new Intern(internInfo.name, internInfo.id, internInfo.email, internInfo.school);
+            employeeInfo.push(intern);
+        }).then(promptAdd)
+}
+
+const promptAdd = () => {
+    inquirer.prompt({
         type: 'list',
         name: 'options',
         message: 'Would you like to add more team members?',
         choices: ['Add Engineer', 'Add Intern', 'Finish Building Team']
+    }).then((data) => {
+    if (data.options === 'Add Engineer') {
+        promptEngineer();
+    } else if (data.options === 'Add Intern') {
+        promptIntern();
+    } else {
+        console.log(employeeInfo);
     }
-];
-
-const promptUser = () => {
-    return inquirer.prompt(employeeQuestions)
-}
-
-const promptManager = () => {
-    return inquirer.prompt(managerQuestion)
-}
-
-const promptEngineer = () => {
-    return inquirer.prompt(engineerQuestion)
-}
-
-const promptIntern = () => {
-    return inquirer.prompt(internQuestion)
-}
-
-const promptAdd = () => {
-    return inquirer.prompt(addInfo)
-}
-
-promptUser()
-    .then(promptManager)
-    .then(promptAdd => {
-        if(options === 'Add Engineer') {
-            promptUser()
-            .then(promptEngineer)
-            .then(promptAdd)
-        } else if(options === 'Add Intern') {
-            promptUser()
-            .then(promptIntern)
-            .then(promptAdd)
-        } else {
-            fs.writeFile(templateData)
-        }
+    return;
     })
+}
+
+promptManager();
