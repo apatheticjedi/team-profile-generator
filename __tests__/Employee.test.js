9c671a4269c84c7c19b5ab2f20af865cf17666c4
Duder <thedudeabides1981@hotmail.com>
2022-09-18 13:05:44 -0700

    added employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,5 +6,29 @@ test('creates an employee object', () => {
     expect(employee.name).toBe('Dave');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toBe(expect.any(String));
+    expect(employee.role).toBe('Employee');
 });
 
+test("gets employee's name", () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getName()).toBe(expect.any(String));
+});
+
+test("gets employee's email", () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining('@'));
+});
+
+test("gets employee's id", () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getId()).toBe(expect.any(Number));
+});
+
+test("gets employee's role", () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getRole()).toBe('Employee');
+});
